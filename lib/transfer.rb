62d51d98e50fdacf2f6e77cc11b8c44cb30454b2
Tania Whiting <tanwhiting@aol.com>
2018-12-19 17:41:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,8 +26,8 @@ class Transfer
   
   def reverse_transfer
     if self.status = "complete"
-      self.sender.balance = self.sender.balance + self.amount
-      self.receiver.balance = self.receiver.balance - self.amount
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
     end
   end
 end
