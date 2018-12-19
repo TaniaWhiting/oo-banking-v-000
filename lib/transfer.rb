7a173afc3869170b22a3823b1f48d9fea943b300
Tania Whiting <tanwhiting@aol.com>
2018-12-19 17:30:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       self.sender.balance -= self.amount
       self.status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
+      self.status = "Transaction rejected. Please check your account balance."
     end
   end
   
